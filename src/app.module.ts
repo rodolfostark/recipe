83cee4286c00fb0796066da9e7c9e776215c2380
rodolfostark <rodolfojonatas@outlook.com>
2024-03-04 23:26:17 -0300

    feat: create recipe model CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { RecipeModule } from './recipe/recipe.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RecipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
